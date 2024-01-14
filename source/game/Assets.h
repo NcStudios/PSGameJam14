401ba6ad7896c617cfc2397137e07c92008a5d40
Jaremie Romer <33360084+jaremieromer@users.noreply.github.com>
2024-01-14 17:25:30 -0600

    Added first tree and first terrain block/hull collider. (#13)

--- source/game/Assets.h
+++ source/game/Assets.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "ncengine/graphics/MeshRenderer.h"
+#include "ncengine/graphics/ToonRenderer.h"
 
 namespace nc::config
 {
@@ -11,34 +11,42 @@ namespace game
 {
 void LoadAssets(const nc::config::AssetSettings& settings);
 
+/** Colliders */
+constexpr auto Terrain01Collider = "terrain01_collider.nca";
+
 /** SFX */
 constexpr auto ForestAmbience = "forest.nca";
 
 /** Meshes */
+constexpr auto Terrain01Mesh = "terrain01.nca";
 constexpr auto TreeMesh = "tree.nca";
+constexpr auto Tree01Mesh = "tree01.nca";
 constexpr auto BusFrontMesh = "bus_front.nca";
 constexpr auto BusCarMesh = "bus_car.nca";
 
 /** Textures */
+constexpr auto Terrain01Texture = "terrain01_base_color.nca";
+constexpr auto Tree01Texture = "tree01_base_color.nca";
 constexpr auto BusFrontTexture = "bus_base_color.nca";
 constexpr auto BusCarTexture = "bus_car_base_color.nca";
 constexpr auto HatchTexture = "hatch.nca";
 constexpr auto OverlayTexture = "overlay.nca";
 
 /** Materials */
-inline static auto BusFrontMaterial = nc::graphics::PbrMaterial
+inline static auto BusFrontMaterial = nc::graphics::ToonMaterial
 {
     .baseColor = BusFrontTexture,
-    .normal = nc::asset::DefaultNormal,
-    .roughness = OverlayTexture,
-    .metallic = nc::asset::DefaultMetallic
+    .overlay = nc::asset::DefaultBaseColor,
+    .hatching = nc::asset::DefaultBaseColor,
+    .hatchingTiling = 6
 };
 
-inline static auto BusCarMaterial = nc::graphics::PbrMaterial
+inline static auto BusCarMaterial = nc::graphics::ToonMaterial
 {
     .baseColor = BusCarTexture,
-    .normal = nc::asset::DefaultNormal,
-    .roughness = OverlayTexture,
-    .metallic = nc::asset::DefaultMetallic
+    .overlay = nc::asset::DefaultBaseColor,
+    .hatching = nc::asset::DefaultBaseColor,
+    .hatchingTiling = 6
 };
+
 } // namespace game
