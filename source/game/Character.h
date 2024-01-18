87fd627d742c341342a78fd5d15fbe2b385d9748
McCallisterRomer <47375742+McCallisterRomer@users.noreply.github.com>
2024-01-18 05:47:07 -0600

    character movement and purify improvements (#26)
    * add inching movement; update purify collider
    
    * ignore double disinfect

--- source/game/Character.h
+++ source/game/Character.h
@@ -11,9 +11,10 @@ class CharacterController : public nc::FreeComponent
     public:
         static constexpr auto fixedDt = 0.01667f;
         static constexpr auto moveAcceleration = 0.4f;
-        static constexpr auto maxMoveVelocity = 8.0f;
+        static constexpr auto moveVelocityUpperBound = 8.0f;
+        static constexpr auto moveVelocityLowerBound = 2.0f;
         static constexpr auto turnAcceleration = 0.05f;
-        static constexpr auto maxTurnVelocity = 2.0f;
+        static constexpr auto maxTurnVelocity = 1.0f;
         static constexpr auto jumpForce = 30.0f;
         static constexpr auto jumpCooldown = 0.35f;
         static constexpr auto purifyCooldown = 1.0f;
@@ -26,9 +27,11 @@ class CharacterController : public nc::FreeComponent
     private:
         nc::Entity m_purifier = nc::Entity::Null();
         float m_currentMoveVelocity = 0.0f;
+        float m_timeAtMoveBound = 0.0f;
         float m_currentTurnVelocity = 0.0f;
         float m_jumpRemainingCooldownTime = 0.0f;
         float m_purifyRemainingCooldownTime = 0.0f;
+        bool m_inchDecelerating = false;
         bool m_jumpOnCooldown = false;
         bool m_purifyOnCooldown = false;
 
