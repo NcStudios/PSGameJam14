13dd8304d4abe4aae91fc5ec282546373040607f
McCallisterRomer <47375742+McCallisterRomer@users.noreply.github.com>
2024-01-18 09:28:40 -0600

    quest trigger + fps tracker (#32)
    * add quest trigger
    
    * fps tracker

---
+++ source/game/QuestTrigger.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Core.h"
+#include "Event.h"
+
+namespace game
+{
+inline auto AttachQuestTrigger(nc::ecs::Ecs world,
+                               nc::Entity parent,
+                               Event onPlayerHit,
+                               const nc::Vector3& center = nc::Vector3::Zero(),
+                               const nc::Vector3& extents = nc::Vector3::One()) -> nc::Entity
+{
+    auto trigger = world.Emplace<nc::Entity>({
+        .parent = parent,
+        .tag = tag::QuestTrigger,
+        .layer = layer::QuestTrigger,
+        .flags = nc::Entity::Flags::NoSerialize | nc::Entity::Flags::Static
+    });
+
+    // placeholder indicator, maybe plane with some texture or particle emitter?
+    world.Emplace<nc::graphics::ToonRenderer>(trigger, nc::asset::PlaneMesh);
+
+    world.Emplace<nc::physics::Collider>(trigger, nc::physics::BoxProperties{.center = center, .extents = extents}, true);
+    auto triggerHandler = [onPlayerHit](nc::Entity self, nc::Entity other, nc::Registry* registry)
+    {
+        if (other.Layer() != layer::Character)
+            return;
+
+        FireEvent(onPlayerHit);
+        registry->Remove<nc::Entity>(self);
+    };
+
+    world.Emplace<nc::CollisionLogic>(trigger, nullptr, nullptr, triggerHandler, nullptr);
+    return trigger;
+}
+} // namespace game
