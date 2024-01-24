36fc70ea5fd750f726cb51edc326ac8b47318806
McCallisterRomer <47375742+McCallisterRomer@users.noreply.github.com>
2024-01-24 17:50:01 -0600

    Cal/update trees and menu (#59)
    * add more trees; update spread
    
    * remove 'resume' from end game menu

--- source/game/UI.h
+++ source/game/UI.h
@@ -25,6 +25,7 @@ class GameUI : public nc::ui::IUI,
         void OpenMenu() { m_menuOpen = true; }
         void ToggleTreeCounter(bool isOpen) { m_counterOpen = isOpen; }
         void SetTreeCounts(size_t healthy, size_t infected) { m_healthyCount = healthy; m_infectedCount = infected; }
+        void SetMenuToEndGameMenu() { m_enableEndGameMenu = true; }
 
     private:
         std::function<void()> m_stopEngine; // could add event instead
@@ -33,12 +34,14 @@ class GameUI : public nc::ui::IUI,
         size_t m_currentDialogNextCharacter = 0;
         std::string m_currentDialog = "";
         bool m_menuOpen = false;
+        bool m_enableEndGameMenu = false;
 
         bool m_counterOpen = false;
         size_t m_healthyCount = 0;
         size_t m_infectedCount = 0;
 
         void DrawMainMenu();
+        void DrawEndGameMenu();
         void DrawDialogWindow();
         void DrawTreeCounter();
         void SetDialogPosition(size_t pos);
